e22d797613b524d27ef337a84511fc114af024f2
IranildoAraujo <iranildo.ramos@yahoo.com.br>
2024-12-01 20:08:18 -0300

    refactor: AUT-2 removendo comentários

--- src/main/java/com/authservice/config/WebConfig.java
+++ src/main/java/com/authservice/config/WebConfig.java
@@ -29,27 +29,11 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		var allowedOrigins = corsOriginPatterns.split(",");
 		registry.addMapping("/**")
-				// .allowedMethods("GET", "POST", "PUT")
 				.allowedMethods("*").allowedOrigins(allowedOrigins).allowCredentials(true);
 	}
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		// https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
-		// Via EXTENSION. http://localhost:8080/api/person/v1.xml DEPRECATED on
-		// SpringBoot 2.6
-
-		// Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
-		/*
-		 * configurer.favorParameter(true)
-		 * .parameterName("mediaType").ignoreAcceptHeader(true)
-		 * .useRegisteredExtensionsOnly(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
-
-		// Via HEADER PARAM. http://localhost:8080/api/person/v1
-
 		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
 				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
 				.mediaType("xml", MediaType.APPLICATION_XML).mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
